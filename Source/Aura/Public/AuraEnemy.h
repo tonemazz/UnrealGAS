c1b1e3630e70e446c7d4651dba14ce63e9d8a2fa
Tony Mazich <128867937+tonemazz@users.noreply.github.com>
2025-01-12 21:47:44 -0800

    Added goblin_spear and goblin_slingshot, ABPs for Aura and Goblins, ABP template for enemy

---
+++ Source/Aura/Public/AuraEnemy.h
@@ -0,0 +1,17 @@
+// Tone
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
