43909af3e0813cf93d18027521769244acf337e6
Tony Mazich <128867937+tonemazz@users.noreply.github.com>
2025-01-12 20:58:56 -0800

    Added AuraCharacter and AuraEnemy

--- Source/Aura/Private/AuraCharacterBase.cpp
+++ Source/Aura/Private/AuraCharacterBase.cpp
@@ -6,25 +6,13 @@
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
-	PrimaryActorTick.bCanEverTick = true;
-
+	PrimaryActorTick.bCanEverTick = false;
 }
 
 void AAuraCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-void AAuraCharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 
-}
-
-void AAuraCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
